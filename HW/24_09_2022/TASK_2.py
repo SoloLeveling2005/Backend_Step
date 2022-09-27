18ea32c1528b4f809417094f658490eefb4e5d65
SoloLeveling <SoloLevelingMansur2005@gmail.com>
2022-09-27 09:12:27 +0600

    27_09_2022

--- HW/24_09_2022/TASK_2.py
+++ HW/24_09_2022/TASK_2.py
@@ -2,9 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+
+
 @app.route("/")
 def hello_world():
     text = ""
     for i in range(1,667):
         text += f"<p>{i}</p>"
     return text
+
+
+
