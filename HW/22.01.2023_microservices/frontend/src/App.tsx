f6f8a98b4060522edfe8c6cf99fa0dfca763549a
SoloLeveling <SoloLevelingMansur2005@gmail.com>
2023-01-15 16:50:09 +0600

    15.01.2023

--- HW/22.01.2023_microservices/frontend/src/App.tsx
+++ HW/22.01.2023_microservices/frontend/src/App.tsx
@@ -9,32 +9,36 @@ import {
 
 function App() {
   return (
-    <Router>
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Главная</Link>
-          </li>
-          <li>
-            <Link to="/about">Контакты</Link>
-          </li>
-          <li>
-            <Link to="/users">Пользователи</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+  <main>
+    Hello
 
-    <main>
-      {/* <Switch> рендерит первый <Route>, совпавший с URL */}
-        <Routes>
-          <Route path="/about" element={<h1>about</h1>} />
-          <Route path="/users" element={<h1>users</h1>} />
-          <Route path="/" element={<h1>home</h1>} />
-        </Routes>
+{/*     <Router> */}
+{/*     <header> */}
+{/*       <nav> */}
+{/*         <ul> */}
+{/*           <li> */}
+{/*             <Link to="/">Главная</Link> */}
+{/*           </li> */}
+{/*           <li> */}
+{/*             <Link to="/about">Контакты</Link> */}
+{/*           </li> */}
+{/*           <li> */}
+{/*             <Link to="/users">Пользователи</Link> */}
+{/*           </li> */}
+{/*         </ul> */}
+{/*       </nav> */}
+{/*     </header> */}
+
+{/*     <main> */}
+{/*        */}{/* <Switch> рендерит первый <Route>, совпавший с URL */}
+{/*         <Routes> */}
+{/*           <Route path="/about" element={<h1>about</h1>} /> */}
+{/*           <Route path="/users" element={<h1>users</h1>} /> */}
+{/*           <Route path="/" element={<h1>home</h1>} /> */}
+{/*         </Routes> */}
+{/*     </main> */}
+{/*   </Router> */}
     </main>
-  </Router>
   );
 }
 
