e6e368ceb7fb4834b12e0e9c07a1de7932a28024
SoloLeveling <SoloLevelingMansur2005@gmail.com>
2023-02-02 22:30:02 +0600

    02.02.2023

--- HW/30.01.2023/frontend/src/pages/ToDo.tsx
+++ HW/30.01.2023/frontend/src/pages/ToDo.tsx
@@ -6,7 +6,13 @@ import './../App.css';
 function ToDo() {
   return (
     <div className="App">
-        Home
+        <form action={''} method={'POST'}>
+            <div className="mb-3">
+                <label htmlFor="exampleInputTitle" className="form-label">Введите название задачи</label>
+                <input type="text" className="form-control" id="exampleInputTitle" aria-describedby="emailHelp"/>
+            </div>
+            <button type="submit" className="btn btn-primary w-100">Добавить</button>
+        </form>
     </div>
 
   );
