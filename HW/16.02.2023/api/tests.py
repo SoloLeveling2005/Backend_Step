35e727f704e1d64395cbe7f466f76a3a555b0390
SoloLeveling <SoloLevelingMansur2005@gmail.com>
2023-02-26 21:29:32 +0600

    26.02.2023

--- HW/16.02.2023/api/tests.py
+++ HW/16.02.2023/api/tests.py
@@ -1,3 +1,67 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+
+# # Create your tests here.
+#
+#
+class GetMethodTest(TestCase):
+    def setUp(self):
+        print("\nGetMethodTest")
+
+    def test(self):
+        """
+        Получение данных с БД
+        """
+
+        client = Client()
+        response = client.get('/get/')
+
+        self.assertEqual(response.status_code, 202)
+
+class PostMethodTest(TestCase):
+    def setUp(self):
+        print("\nPostMethodTest")
+
+    def test(self):
+        """
+        Добавление данных в БД
+        """
+
+        title = "Думай и богатей"
+        client = Client()
+        response = client.post('/post/', {'title': title})
+
+        self.assertEqual(response.status_code, 200)
+
+
+class UpdateMethodTest(TestCase):
+    def setUp(self):
+        print("\nUpdateMethodTest")
+
+    def test(self):
+        """
+        Обновление данных в БД
+        """
+
+        book_id = 1
+        client = Client()
+        response = client.post('/update/', {'id': book_id})
+
+        self.assertEqual(response.status_code, 200)
+
+
+
+class DeleteMethodTest(TestCase):
+    def setUp(self):
+        print("\nDeleteMethodTest")
+
+    def test(self):
+        """
+        Удаление данных из БД
+        """
+        book_id = 0
+        client = Client()
+        response = client.post('/delete/', {'id': book_id})
+
+        self.assertEqual(response.status_code, 200)
--- HW/16.02.2023/api/views.py
+++ HW/16.02.2023/api/views.py
@@ -1,3 +1,45 @@
+import json
+
+from django.http import JsonResponse
 from django.shortcuts import render
 
+from api.models import Book
+
+
 # Create your views here.
+
+
+def getDB(requests):
+    data = [{'id': x.id, 'title': x.title} for x in Book.objects.all()]
+    return JsonResponse(data=data, safe=False, status=202)
+
+
+def postDB(requests):
+    try:
+        title = requests.POST.get('title')
+        Book.objects.create(title=title)
+        return JsonResponse({'status': 'ok'}, status=200)
+    except Exception as e:
+        return JsonResponse({'error': e}, status=204)
+
+
+def updateDB(requests):
+    try:
+        book_id = requests.POST.get('id')
+        book = Book.objects.get(id=int(book_id))
+        book.title = "Джек Лондон"
+        book.save()
+        return JsonResponse({'status': 'ok'}, status=200)
+    except Exception as e:
+        print(e)
+        return JsonResponse({'error': 'Нет данных'}, status=204)
+
+
+def deleteDB(requests):
+    try:
+        book_id = requests.POST.get('id')
+        Book.objects.get(id=int(book_id)).delete()
+        return JsonResponse({'status': 'ok'}, status=200)
+    except Exception as e:
+        print(e)
+        return JsonResponse({'error': 'Нет данных'}, status=204)
