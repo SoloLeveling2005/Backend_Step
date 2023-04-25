9f5bb9ff90685213a29d2afff734df330eab3d03
SoloLeveling <SoloLevelingMansur2005@gmail.com>
2023-04-25 07:39:30 +0600

    Подведение итогов/Доработка шаблона и контроллера

--- HW/ProfileReview/app/views.py
+++ HW/ProfileReview/app/views.py
@@ -16,12 +16,20 @@ def main(request):
     user = Profile.objects.get(username=username)
     profiles_reviews = [profile.estimations() for profile in profiles]
     # Конструкция не очень, потом заменю 😅
-    new_profiles_reviews = []
-    for i in profiles_reviews:
-        for x in i:
-            new_profiles_reviews.append(x)
-
-    return render(request, 'main.html', context={'form': form, 'profiles': new_profiles_reviews, 'user': user})
+    # {1:{profile:profile, estimations:[], my_estimation:False}}
+    new_profiles = {}
+    for profile in profiles:
+        id_ = profile.id
+        print(id_)
+        new_profiles[id_] = {'profile': profile, 'estimations': [], 'my_estimation': False}
+        for estimation in profile.estimations():
+            new_profiles[id_]['estimations'].append(estimation)
+            if estimation.user == user:
+                new_profiles[id_]['my_estimation'] = True
+    print(new_profiles)
+    return render(request, 'main.html',
+                  context={'form': form, 'profiles': profiles, 'new_profiles': new_profiles,
+                           'user': user})
 
 
 def add_profile(request):
@@ -46,7 +54,9 @@ def new_estimation(request):
         return redirect('add_profile')
 
     print(request.POST)
-    estimation = request.POST.get('estimation')
+    profile_id = request.POST.get('profile')
+    profile = Profile.objects.get(id=profile_id)
     user = Profile.objects.get(username=username)
-    Reviews(estimation=estimation, profile=user).save()
+    estimation = request.POST.get('estimation')
+    Reviews(estimation=estimation, profile=profile, user=user).save()
     return redirect('main')
